1b541990ac152589b1b713c56b6d2499242d6c22
Guy Whale <guywhale@gmail.com>
2022-11-09 15:54:13 +0000

    Use a Blade component to set up layout

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,18 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     <article>
         <h1>{{ $post->title }}</h1>
         <p>{{ $post->date }}</p>
         <div>{!! $post->body !!}</div>
     </article>
     <a href="/">Go Back</a>
-</body>
-</html>
+</x-layout>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,13 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>My Blog</title>
-    <link rel="stylesheet" href="/app.css">
-</head>
-<body>
+<x-layout>
     @foreach ($posts as $post)
         <article class="{{ $loop->even ? 'foo' : 'bar' }}">
             <h1>
@@ -19,5 +10,4 @@
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
-</body>
-</html>
+</x-layout>
